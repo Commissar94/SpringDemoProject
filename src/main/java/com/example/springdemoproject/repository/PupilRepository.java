fb10bf750f94089bfe01bd1eacb90ab097f0496e
Commissar94 <andreykomissarov94@gmail.com>
2021-07-14 15:55:07 +0300

    error with specification

--- src/main/java/com/example/springdemoproject/repository/PupilRepository.java
+++ src/main/java/com/example/springdemoproject/repository/PupilRepository.java
@@ -1,7 +1,9 @@
 package com.example.springdemoproject.repository;
 
 import com.example.springdemoproject.data.Pupil;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PupilRepository extends CrudRepository<Pupil,Long> {
+public interface PupilRepository extends JpaRepository<Pupil, Long>, JpaSpecificationExecutor<Pupil> {
 }
